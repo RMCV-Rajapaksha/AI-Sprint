89811a1fba104e4001c3f6458991d37ad2673ff4
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-01-21 22:44:48 +0530

    Update frontend components with date picker and chart visualizations; add new data sources and enhance UI layout

---
+++ Dashboard/frontend/src/data/data.js
@@ -0,0 +1,24 @@
+export const data = [
+    ["Country", "Sales"],
+    ["Germany", 200],
+    ["United States", 300],
+    ["Brazil", 400],
+    ["Canada", 500],
+    ["France", 600],
+    ["RU", 700],
+  ];
+
+  export const dataDonut = [
+    ["Items", "Sales"],
+    ["Mango", 11],
+    ["Tomato", 2],
+    ["Strawberry", 2],
+    ["Watermelon", 2],
+    ["Banana", 7], // CSS-style declaration
+  ];
+  
+  export const optionsDonut = {
+
+    pieHole: 0.4,
+    is3D: false,
+  };
