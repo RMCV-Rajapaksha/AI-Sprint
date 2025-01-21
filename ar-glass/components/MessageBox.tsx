112f5b701b63ad7315ae38576378622e8a8edd47
MalithaJeewaka <144432834+MalithaJeewaka@users.noreply.github.com>
2025-01-21 21:36:27 +0530

    inbox and notification components partially done

--- ar-glass/components/MessageBox.tsx
+++ ar-glass/components/MessageBox.tsx
@@ -1,18 +1,29 @@
 import React from "react";
 import MessageBubble from "./MessageBubble";
 
+const messages = [
+  "System message 1",
+  "System message 2",
+  "System message 3",
+  "System message 4",
+  "System message 5",
+  "System message 6",
+  "System message 7",
+];
+
 const MessageBox = () => {
   return (
     <div className="absolute bottom-5 left-5">
-      <div className="bg-slate-700 w-[500px] h-[200px] px-3 py-3 flex flex-col justify-between gap-1 rounded-2xl">
-        <div className="flex-1 relative w-full h-full overflow-y-auto">
-          <MessageBubble type="worker" />
-          <MessageBubble type="system" />
+      <div className="backdrop-blur-sm border border-slate-400 w-[500px] h-[250px] px-5 py-3 flex flex-col justify-between gap-1 rounded-2xl">
+        <div className="flex-1 inline-flex flex-col gap-4 relative w-full h-full overflow-y-auto custom-scrollbar">
+          {messages.map((message, i) => (
+            <MessageBubble key={i} message={message} type="system" />
+          ))}
         </div>
-        <input
+        {/* <input
           type="text"
           className="rounded-2xl w-full h-10 px-2 py-2 outline-none"
-        />
+        /> */}
       </div>
     </div>
   );
--- ar-glass/components/MessageBubble.tsx
+++ ar-glass/components/MessageBubble.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const MessageBubble = ({ type }: { type: string }) => {
+const MessageBubble = ({
+  type,
+  message,
+}: {
+  type: string;
+  message: string;
+}) => {
   if (type === "worker") {
     return (
       <div className=" bg-slate-500 absolute right-0 inline-flex px-5 text-white rounded-3xl mt-6">
@@ -9,8 +15,8 @@ const MessageBubble = ({ type }: { type: string }) => {
     );
   } else if (type === "system") {
     return (
-      <div className=" inline-flex px-5 bg-blue-600 text-white rounded-3xl">
-        Message from system
+      <div className=" inline-flex px-5 py-1 bg-blue-600 text-white rounded-3xl w-4/5">
+        {message}
       </div>
     );
   }
