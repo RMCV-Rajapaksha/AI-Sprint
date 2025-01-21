04f3a5d4f75c0b7efa40f6b61fe6d48ff599a14d
MalithaJeewaka <144432834+MalithaJeewaka@users.noreply.github.com>
2025-01-21 23:56:04 +0530

    notification and message animations and components with trigger fuctions

--- ar-glass/components/Arglass.tsx
+++ ar-glass/components/Arglass.tsx
@@ -1,26 +1,39 @@
 "use client";
 
-import React, { useRef, useCallback, useEffect } from "react";
+import React, { useRef, useCallback, useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import jsQR from "jsqr";
 import { BrowserMultiFormatReader } from "@zxing/library";
 import Image from "next/image"; // Import Image from next/image
-import Message from "./Message";
+import Message from "./Notification";
 import Controlbox from "./Controlbox";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import MessageBox from "./MessageBox";
 import AlertBox from "./AlertBox";
 import Button from "./Button";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import Notification from "./Notification";
 
 const Webcam = dynamic(() => import("react-webcam"), { ssr: false });
 
-const messageContent =
-  " Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, optio.";
+// const notificationMessage = "This is a notification";
+
+// const alert = "This is an alert";
+
+// const message = "This is a message";
 
 const Arglass = () => {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+
+  const [notification, setNotification] = useState("This is a notification");
+  // const [notificationMessage, setNotificationMessage] = useState("");
+  const [alert, setAlert] = useState("This is a message");
+  // const [alertMessage, setAlertMessage] = useState("");
+  const [message, setMessage] = useState("This is a message");
+  // const [messageText, setMessageText] = useState("");
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -60,9 +73,13 @@ const Arglass = () => {
     return () => clearInterval(interval);
   }, [capture]);
 
-  const sendMessage = () => {};
-  const sendAlert = () => {};
-  const sendNotification = () => {};
+  const sendMessage = (message: string) => {};
+  const sendAlert = (alert: string) => {
+    setAlert(alert);
+  };
+  const sendNotification = (notification: string) => {
+    setNotification(notification);
+  };
 
   return (
     <div className="h-[800px] relative mx-6  bg-[url('/glass/warehouse.jpg')] bg-cover bg-center ">
@@ -78,21 +95,24 @@ const Arglass = () => {
           <Image src={"/glass/map.png"} width={300} height={300} alt="map" />
         </div>
 
-        <Message message={messageContent} />
+        <Notification notificationMessage={notification} />
         <Controlbox />
         <MessageBox />
       </div>
-      <AlertBox alert="This is an alert" />
+      <AlertBox alert={alert} />
       <div className="absolute bottom-5 left-1/2 translate-x-[-50%] flex gap-3">
-        <Button className="bg-blue-500" onClick={() => sendMessage()}>
+        <Button className="bg-blue-500" onClick={() => sendMessage(message)}>
           Message
         </Button>
-        <Button className="bg-red-500" onClick={() => sendAlert()}>
+        <Button
+          className="bg-red-500"
+          onClick={() => sendAlert("New alert sent")}
+        >
           Alert
         </Button>
         <Button
           className="bg-slate-100 text-black"
-          onClick={() => sendNotification()}
+          onClick={() => sendNotification("New notification message sent")}
         >
           Notify
         </Button>
